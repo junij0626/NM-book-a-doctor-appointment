317d5a6c32a8e8b55b86e4269b803d15ebaf3bc6
junij0626 <141354053+junij0626@users.noreply.github.com>
2024-11-28 15:26:03 +0530

    Add files via upload
    Signed-off-by: junij0626 <141354053+junij0626@users.noreply.github.com>

---
+++ connectToDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+console.log(process.env.MONGO_DB);
+const connectToDB = () => {
+  mongoose
+    .connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      throw new Error(`Could not connect to MongoDB: ${err}`);
+    });
+};
+
+module.exports = connectToDB;
